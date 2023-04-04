ff1a8001a491c59b0245e05c368b6cae67cd5733
Rayan msheik <100214246+rayanmsheikk@users.noreply.github.com>
2023-04-04 22:48:54 +0300

    Uploaded contact.sh

---
+++ contact.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Read the contents of the contacts.txt file and assign it to a variable
+contact_number=$(cat contacts.txt)
+
+# Print the contact number
+echo "The contact number is: $contact_number"
