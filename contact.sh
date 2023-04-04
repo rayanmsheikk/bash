840c3dc701001603d2019f925d51299b81fcabd7
Rayan msheik <100214246+rayanmsheikk@users.noreply.github.com>
2023-04-04 23:04:54 +0300

    Update contact.sh

--- contact.sh
+++ contact.sh
@@ -2,6 +2,6 @@
 
 # Read the contents of the contacts.txt file and assign it to a variable
 contact_number=$(cat contacts.txt)
-do echo"Calling $contact_number" termux-telephony-call $contact_number done < contacts.txt
+termux-telephony-call $contact_number
 # Print the contact number
 echo "The contact number is: $contact_number"
